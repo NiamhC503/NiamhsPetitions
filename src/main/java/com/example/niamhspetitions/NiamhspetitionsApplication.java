c2c246a35e862742e066c78eccd2db71eef05fbd
NiamhC503 <n.connaghan1@universityofgalway.ie>
2023-11-21 13:44:37 +0000

    commit 1

---
+++ src/main/java/com/example/niamhspetitions/NiamhspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.niamhspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiamhspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiamhspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/niamhspetitions/NiamhspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niamhspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiamhspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
