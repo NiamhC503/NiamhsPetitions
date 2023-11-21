8283f0e91eea720f169ee57fc5b968e454406fe6
NiamhC503 <n.connaghan1@universityofgalway.ie>
2023-11-21 13:50:19 +0000

    Commit editing pom making war file not include version Adding index.html page as home page of app

--- src/main/java/com/example/niamhspetitions/NiamhspetitionsApplication.java
+++ src/main/java/com/example/niamhspetitions/NiamhspetitionsApplication.java
@@ -2,9 +2,18 @@ package com.example.niamhspetitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class NiamhspetitionsApplication {
+
+	//presents home page at path root
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NiamhspetitionsApplication.class, args);
